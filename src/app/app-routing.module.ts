bc1201837491d156df43691e98720356e60eeaf9
dEvil371 <adityasahoo1999@gmail.com>
2023-01-15 20:50:53 +0530

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { MainComponent } from './main/main.component';
+import { Main2Component } from './main2/main2.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'page1',component:MainComponent
+  },
+  {
+    path:'page2',component:Main2Component
+  },
+  {
+    path:'form',component:FormComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
